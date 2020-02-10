db0d99262bd91b4bfc96cf8011f6f1df4af729f1
Albert Diaz <diazalbert45@gmail.com>
2020-02-10 12:05:10 -0400

    primer commit

--- src/app/pages/peli-details/peli-details.page.spec.ts
+++ src/app/pages/peli-details/peli-details.page.spec.ts
@@ -1,19 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
-import { HomePage } from './home.page';
+import { PeliDetailsPage } from './peli-details.page';
 
-describe('HomePage', () => {
-  let component: HomePage;
-  let fixture: ComponentFixture<HomePage>;
+describe('PeliDetailsPage', () => {
+  let component: PeliDetailsPage;
+  let fixture: ComponentFixture<PeliDetailsPage>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomePage ],
+      declarations: [ PeliDetailsPage ],
       imports: [IonicModule.forRoot()]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(HomePage);
+    fixture = TestBed.createComponent(PeliDetailsPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
